fa5b0a713df5762ac37833b6ab8b7c447c5b74a1
zeshantahir105 <zeshantahir105@gmail.com>
2022-08-25 10:16:21 +0500

    a

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
-
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div `
+
+ margin: 0.5rem 0;
+
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -13,9 +42,7 @@ const CourseInput = props => {
   };
 
   const emptyCheckHandler = (e)=>{
-   if (enteredValue.trim().length === 0) {
-    setIsValid(false);
-  }else{
+   if (enteredValue.trim().length >= 0) {
     setIsValid(true);
   } 
   }
@@ -30,10 +57,10 @@ const CourseInput = props => {
   };
   return (
     <form onSubmit={formSubmitHandler} onChange={emptyCheckHandler}>
-      <div className="form-control">
+      <FormControl className="form-control">
         <label>Course Goal</label>
         <input type="text" className = {!isValid ? "red-bordered" : ""} onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <p className={'error-msg red ' + (isValid ? "hidden" : "")}>You have empty goal, hunh?</p>
       <Button type="submit">Add Goal</Button>
     </form>
